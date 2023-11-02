c6ce3b32398f74104523635733b8ad8818a20942
ShengBin-101 <shengbin.chan@gmail.com>
2023-11-02 19:22:35 +0800

    Backup

--- install/camera_info_manager/share/camera_info_manager/package.sh
+++ install/camera_info_manager/share/camera_info_manager/package.sh
@@ -80,7 +80,6 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/camera_info_manager/hook/cmake_prefix_path.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/camera_info_manager/hook/ld_library_path_lib.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/camera_info_manager/local_setup.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
